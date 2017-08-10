972ba2aed866b533df4f7d6847565132eb3bd7eb
sagicary <sagicary@gmail.com>
2017-08-10 10:38:17 -0400

    Arreglado gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
-var webserver = require('gulp-webserver');
 gulp.task('script', function(){
 	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
 		.pipe(concat('script.js'))
