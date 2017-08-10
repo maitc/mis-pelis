5269b044cfdf35f3db51d8e4f292a5f8c6fed74c
sagicary <sagicary@gmail.com>
2017-08-10 15:20:28 -0400

    Probando localstorage

--- dist/js/script.js
+++ dist/js/script.js
@@ -20261,3 +20261,22 @@ $('#cuenta').click(function(){
 	document.location.href = "cuenta.html";
 })
 /*FIN INDEX*/
+
+/*LOGIN*/
+
+/*FIN LOGIN*/
+
+/*CUENTA*/
+$('.botoncuenta').click(function(){
+	var nombre = $('#name').val();
+	console.log(nombre);
+	localStorage.setItem('name',nombre);
+	var usuario = $('#username').val();
+	console.log(usuario);
+	localStorage.setItem('username',usuario);
+	var correo = $('#mail').val();
+	console.log(correo);
+	localStorage.setItem('mail',correo);
+	console.log(localStorage);
+})
+/*FIN CUENTA*/
