cd80ef2899fdf7c749625c98b7036e0fe80a05d1
sagicary <sagicary@gmail.com>
2017-08-10 13:03:00 -0400

    Welcome listo

---
+++ assets/js/cuenta.js
@@ -0,0 +1,8 @@
+/*INDEX*/
+$('#login').click(function(){
+	document.location.href = "login.html";
+})
+$('#cuenta').click(function(){
+	document.location.href = "cuenta.html";
+})
+/*FIN INDEX*/
--- dist/js/script.js
+++ dist/js/script.js
@@ -20252,3 +20252,12 @@ if (jQuery) {
     $.error('Method ' + methodOrOptions + ' does not exist on jQuery.tap-target');
   };
 })(jQuery);
+
+/*INDEX*/
+$('#login').click(function(){
+	document.location.href = "login.html";
+})
+$('#cuenta').click(function(){
+	document.location.href = "cuenta.html";
+})
+/*FIN INDEX*/
