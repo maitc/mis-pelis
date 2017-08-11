270c940d0fc629b782a195fc31e968567760b444
ValLR <valentinalopezrecabal@gmail.com>
2017-08-10 20:50:22 -0400

    funciones que rescatan valores de la api

--- dist/js/script.js
+++ dist/js/script.js
@@ -20257,3 +20257,70 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('select').material_select();
 });
+$(document).ready(function(){
+
+	$("#parameter").change(function(){
+		if(($("#parameter").val())==1){
+			var req = new XMLHttpRequest();
+			var URL = "https://netflixroulette.net/api/api.php?";
+			var queryType = "title=";
+			var director = $("#search").val();
+			req.open('GET', URL + queryType + director, true);
+			req.addEventListener("load", function() {
+				var response = JSON.parse(req.responseText);
+				console.log(response.show_title);
+				console.log(response.release_year);
+				console.log(response.category);
+				console.log(response.runtime);
+				console.log(response.director);
+			});
+			req.send(null);
+			$("#search").val("");			
+		}
+		else if(($("#parameter").val())==2){
+			var req = new XMLHttpRequest();
+			var URL = "https://netflixroulette.net/api/api.php?";
+			var queryType = "actor=";
+			var director = $("#search").val();
+			req.open('GET', URL + queryType + director, true);
+			req.addEventListener("load", function() {
+				var response = JSON.parse(req.responseText);
+				console.log(response);
+				response.forEach(function(ele){
+					console.log(ele);
+					var titulo = ele.show_title;
+					var anio = ele.release_year;
+					var categoria = ele.category;
+					var duracion = ele.runtime;
+					var director = ele.director;
+				});	
+			});
+			req.send(null);
+			console.log($("#search").val())
+			$("#search").val("");
+		}
+		else if(($("#parameter").val())==3){			
+			var req = new XMLHttpRequest();
+			var URL = "https://netflixroulette.net/api/api.php?";
+			var queryType = "director=";
+			var director = $("#search").val();
+			req.open('GET', URL + queryType + director, true);
+			req.addEventListener("load", function() {
+				var response = JSON.parse(req.responseText);
+				console.log(response);
+				response.forEach(function(ele){
+					console.log(ele);
+					var titulo = ele.show_title;
+					var anio = ele.release_year;
+					var categoria = ele.category;
+					var duracion = ele.runtime;
+					var director = ele.director;
+				});	
+			});
+			req.send(null);
+			console.log($("#search").val())
+			$("#search").val("");			
+		}
+	});
+
+});
