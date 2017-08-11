366bcfde9a8b79fa7afba67a9755d45ab907f181
maitc <maibeet.torres@gmail.com>
2017-08-11 11:56:39 -0400

    archivos generales

--- dist/js/script.js
+++ dist/js/script.js
@@ -20252,3 +20252,81 @@ if (jQuery) {
     $.error('Method ' + methodOrOptions + ' does not exist on jQuery.tap-target');
   };
 })(jQuery);
+
+$(document).ready(function(){
+	var req = new XMLHttpRequest();
+
+	var URL = "https://netflixroulette.net/api/api.php?";
+	var queryType = "actor=";
+	var query = "John+Travolta";
+
+	req.open('GET', URL + queryType + query, true);
+	req.addEventListener("load", function() {
+		var response = JSON.parse(req.responseText);
+
+		var name = query.replace('+', ' ');
+		var movies = Array.from(response);
+		movies.forEach(function(e){
+			$(".actor-movie").append('<p><i class="material-icons icon-movie">movie</i>' + e.show_title + '</p>')
+		})
+		var nameActor = '<h4>' + name + '</h4>';
+
+		$(".actor-name").append(nameActor);
+
+		
+
+	  
+	});
+	req.send(null);
+})
+
+
+/*MAI*/
+
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+
+
+    var req = new XMLHttpRequest();
+
+	var URL = "https://netflixroulette.net/api/api.php?";
+	var queryType = "title=";
+	var query = "pulp%20fiction";
+
+	req.open('GET', URL + queryType + query, true);
+	req.addEventListener("load", function() {
+		var response = JSON.parse(req.responseText);
+
+		//transormando en array cast.
+		var actores = response.show_cast;
+		var arr = actores.split(',');
+		arr.forEach(function(e, f){
+			var contador = f + 1;
+			$(".details-starring").append('<p class="details-actors" id="actor-' + contador + '">' + e + '</p>');
+		})
+		
+		//append de datos
+		var imgMovie = '<img src="' + response.poster + '" class="imagen-movie">';
+		var rating = '<p>Rating: ' + response.rating + '</p>';
+		var titleMovie = '<h6 id="title-movie">' + response.show_title  + '<span>' + response.category + '</span></h6>';
+		var summaryMovie = '<p>' + response.summary + '</p>';
+		var genresMovie = '<p>Genres: ' + response.category + '</p>';
+		var directorMovie = '<p>Director: ' + response.director + '</p>';
+
+	  	$(".img-movie").append(imgMovie);
+	  	$(".details-rating").append(rating);
+	  	$(".details-title").append(titleMovie);
+	  	$(".details-summary").append(summaryMovie);
+	  	$(".details-genres").append(genresMovie);
+	  	$(".details-director").append(directorMovie);
+
+
+	  	$(".details-actors").click(function(){
+	  		window.location.href = "actors.html";
+	  	})
+
+	  
+	});
+	req.send(null);
+
+});
